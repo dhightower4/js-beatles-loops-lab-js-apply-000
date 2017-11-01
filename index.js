5ff5d115eb030bccbb987dbb684495f2c2040771
Donte Hightower <dontehightower12@gmail.com>
2017-11-01 14:11:12 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,6 @@
+function theBeatlesPlay(musicains, instruments) { 
+  var statements = []
+  for (let i = 0; i < musicians.length; i++) {
+    statements.push(`${musicians[i]} plays the ${instruments[i]}`)
+  }
+}
